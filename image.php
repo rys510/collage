8af32ab541f8083e41a2a325b116210c9b443e5a
rys510 <80494156+rys510@users.noreply.github.com>
2021-03-23 22:33:50 +0900

    Add files via upload

--- image.php
+++ image.php
@@ -1,23 +1,11 @@
-[
-    {
-      "path" : "img/img01.jpg",
-      "name" : "name01",
-    },
-    {
-      "path" : "img/img02.jpg",
-      "name" : "name02",
-    },
-    {
-      "path" : "img/img03.jpg",
-      "name" : "name03",
-    },
-    {
-      "path" : "img/img04.jpg",
-      "name" : "name04",
-    },
-    {
-      "path" : "img/img05.jpg",
-      "name" : "name05",
-    },
-
-  ];
+[{path: "img/img01.jpg",…},…]
+0: {path: "img/img01.jpg"…}
+path: "img/img01.jpg"
+1: {path: "img/img02.jpg",…}
+path: "img/img02.jpg"
+2: {path: "img/img03.jpg",…}
+path: "img/img03.jpg"
+3: {path: "img/img04.jpg",…}
+path: "img/img04.jpg"
+4: {path: "img/img05.jpg",…}
+path: "img/img05.jpg"
