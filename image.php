222190f3f53fae3b0dfebb538ae7e9aa92762cae
rys510 <80494156+rys510@users.noreply.github.com>
2021-03-23 22:29:27 +0900

    Add files via upload

---
+++ image.php
@@ -0,0 +1,23 @@
+[
+    {
+      "path" : "img/img01.jpg",
+      "name" : "name01",
+    },
+    {
+      "path" : "img/img02.jpg",
+      "name" : "name02",
+    },
+    {
+      "path" : "img/img03.jpg",
+      "name" : "name03",
+    },
+    {
+      "path" : "img/img04.jpg",
+      "name" : "name04",
+    },
+    {
+      "path" : "img/img05.jpg",
+      "name" : "name05",
+    },
+
+  ];
